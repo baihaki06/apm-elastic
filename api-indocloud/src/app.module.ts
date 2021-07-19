901e26b4612c2ea5932026dd59ac32d19f28cf37
baihaki06 <baihaki0607@gmail.com>
2021-07-19 16:05:22 +0700

    change config 'npm i elastic-apm-nest'

--- api-indocloud/src/app.module.ts
+++ api-indocloud/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApmModule } from 'elastic-apm-nest';
 
 @Module({
-  imports: [],
+  imports: [ApmModule.forRootAsync()],
   controllers: [AppController],
   providers: [AppService],
 })
--- api-indocloud/src/main.ts
+++ api-indocloud/src/main.ts
@@ -1,41 +1,42 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3020);
-}
-bootstrap();
-
-// import {
-//   APM_MIDDLEWARE,
-//   ApmErrorInterceptor,
-//   ApmHttpUserContextInterceptor,
-//   initializeAPMAgent,
-// } from 'elastic-apm-nest';
-
-// initializeAPMAgent({
-//   serviceName: 'apmhapijs',
- // secretToken: 'VYpMojUjGe77Oe7HmP',
- // serverUrl: 'https://faa6b3a5b0484f358c641362b0f5e63f.apm.us-west1.gcp.cloud.es.io:443',
- // environment: 'production'
-// });
-
 // import { NestFactory } from '@nestjs/core';
 // import { AppModule } from './app.module';
 
 // async function bootstrap() {
 //   const app = await NestFactory.create(AppModule);
-
-//   const apmMiddleware = app.get(APM_MIDDLEWARE);
-//   const globalInterceptors = [
-//     app.get(ApmHttpUserContextInterceptor),
-//     app.get(ApmErrorInterceptor),
-//   ];
-
-//   app.useGlobalInterceptors(...globalInterceptors);
-
-//   app.use(apmMiddleware);
 //   await app.listen(3020);
 // }
 // bootstrap();
+
+import {
+  APM_MIDDLEWARE,
+  ApmErrorInterceptor,
+  ApmHttpUserContextInterceptor,
+  initializeAPMAgent,
+} from 'elastic-apm-nest';
+
+initializeAPMAgent({
+  serviceName: 'apmhapijs',
+  secretToken: 'VYpMojUjGe77Oe7HmP',
+  serverUrl:
+    'https://faa6b3a5b0484f358c641362b0f5e63f.apm.us-west1.gcp.cloud.es.io:443',
+  environment: 'production',
+});
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const apmMiddleware = app.get(APM_MIDDLEWARE);
+  const globalInterceptors = [
+    app.get(ApmHttpUserContextInterceptor),
+    app.get(ApmErrorInterceptor),
+  ];
+
+  app.useGlobalInterceptors(...globalInterceptors);
+
+  app.use(apmMiddleware);
+  await app.listen(3020);
+}
+bootstrap();
